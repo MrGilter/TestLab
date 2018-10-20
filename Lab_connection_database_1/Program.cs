bf64db8638d006a1511fecc58b42c5b8fd78ae61
Mirator <die.arbeit.42@gmail.com>
2018-10-20 12:36:17 +0300

    Update 5

--- Lab_connection_database_1/Program.cs
+++ Lab_connection_database_1/Program.cs
@@ -6,12 +6,59 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab_connection_database_1
 {
     class Program
     {
         static string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        static string sqlExpression = "SELECT * FROM TEST_lab_tab";
+        //connectionString="Data Source=.\SQLEXPRESS;Initial Catalog=Test_lab; User Id = StupidUserTest; Password = 2046"
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Назови файл: ");
+            string fileName = Console.ReadLine();
+            CreateFile(fileName);
+            Console.ReadKey();
+
+        }
+        static void CreateFile(string nameFile)
+        {
+            string route = String.Format("C:\\{0}.txt", nameFile);
+            FileStream file1 = new FileStream(route, FileMode.Create);
+            using (StreamWriter writer = new StreamWriter(file1))
+            {
+                DataTable table = ReadDB();
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        writer.Write(table.Rows[i][j] + "  ");
+                    }
+                    writer.WriteLine();
+                }
+                Console.WriteLine("gotovo blet");
+            }
+        }
+        static DataTable ReadDB()
+        {
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sqlExpression, connection);
+                DataSet set = new DataSet();
+                adapter.Fill(set);
+                DataTable dt = set.Tables[0];
+                return dt;
+            }
+            
+        }
+        
+        void lab_db_6()
+        {
+            /*
+              static string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
         static string sqlExpression = "SELECT * FROM TEST_lab_tab";
 
         static void Main(string[] args)
@@ -123,8 +170,8 @@ namespace Lab_connection_database_1
                             
             return sl.Remove(position2).Substring(position1); 
         }//обрезание теста
-
-
+             */
+        }// работа из строками
         void lab_db_5()
         {
             /*
